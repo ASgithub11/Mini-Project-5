b72f0528b19b78b3c57ba510f70267df6e101c94
Ash <aishasiddiqa151@gmail.com>
2024-07-22 21:33:59 -0700

    import fs/inquirer

---
+++ index.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import inquirer from 'inquirer';
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        message: 'What is your name?',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'What is your location?',
+        name: 'location',
+    },
+    {
+        type: 'input',
+        message: 'What is your bio?',
+        name: 'bio',
+    },
+])
