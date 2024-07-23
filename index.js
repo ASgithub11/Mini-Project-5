f9d729238f685036e3dd898b8e76687c3710f651
Cameron Barfuss <Cameron.barfuss@gmail.com>
2024-07-22 23:16:17 -0600

    Add more inputs and create index.html file with all information

--- index.js
+++ index.js
@@ -18,4 +18,45 @@ inquirer
         message: 'What is your bio?',
         name: 'bio',
     },
+    {
+        type: 'input',
+        message: 'What is your LinkedIn URL?',
+        name: 'linkedIn',
+    },
+    {
+        type: 'input',
+        message: 'What is your Github URL?',
+        name: 'github',
+    },
 ])
+
+    .then((response) => 
+    {   
+        let htmlDocument = 
+        `<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <title>HTML Display</title>
+    </head>
+    <body>
+        <header>
+            <h1>${response.name}</h1>
+            <h2>${response.location}</h2>
+        </header>
+        <section>
+            <h3>${response.bio}</h3>
+            <h3>${response.linkedIn}</h3>
+            <h3>${response.github}</h3>
+        </section>
+    </body>
+</html>`;
+        fs.writeFile('index.html', htmlDocument, err => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log("File Written Successfully");
+            }
+        });
+    }
+)
